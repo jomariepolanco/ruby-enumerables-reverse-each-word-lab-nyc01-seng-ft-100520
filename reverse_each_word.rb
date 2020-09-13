84dbeb6ff4567b8b3cc55a04139cacea961851cd
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-13 18:59:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,6 +11,7 @@ end
 def reverse_each_word(string)
   string_array = string.split
   string_array.collect do |word|
-    word.reverse!.join(" ")
+    word.reverse!
+    string_array.join(" ")
   end 
 end 
